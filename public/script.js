adc34f1f577d031c318b2063c9bddfcff343f046
Bradley <bradley@cmykpluswhite.com>
2017-11-03 16:20:06 -0400

    Replace brand logos with plain text. Attempt to fix brand page refresh crash problem.

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var App = angular.module('CMYKApp', ['ngRoute', 'ngCookies', 'ngTouch']);
+var App = angular.module('CMYKApp', ['ngRoute', 'ngTouch']);
 
 //ROUTER------------------------------------------------------------------------
 App.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
