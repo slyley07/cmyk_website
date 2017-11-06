46b86840f2e616a46a5d3591cbba84eabb3e0270
Sean <sean.sehwan.choi@gmail.com>
2017-11-06 14:13:20 -0500

    fix: Remove modules causing injection error

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-var App = angular.module('CMYKApp', ['ngRoute', 'ngCookies', 'ngTouch']);
+var App = angular.module('CMYKApp', ['ngRoute']);
 
 //ROUTER------------------------------------------------------------------------
 App.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
