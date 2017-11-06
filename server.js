286c60bbb14b2207a4faf8087a789904d22a68b2
Sean <sean.sehwan.choi@gmail.com>
2017-11-06 15:16:06 -0500

    fix: Replaced deprecated method

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(urlencoded({
 app.use(express.static(__dirname + '/'));
 
 app.all('/*', function(req, res) {
-    res.sendfile('index.html');
+    res.sendFile('index.html');
 });
 
 var port = (process.env.PORT || 3000);
